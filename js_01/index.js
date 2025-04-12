52544c0315020eb228955262ac5742b1b152d67e
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-12 11:48:23 +0500

    for loop

--- js_01/index.js
+++ js_01/index.js
@@ -59,8 +59,98 @@
 //   console.log(i);
 // }
 
-var sum = 0;
-for (i = 1; i < 10; i++) {
-  console.log((sum += i));
-  if (sum === 10) break;
-}
+// var sum = 0;
+// for (i = 1; i < 10; i++) {
+//   console.log((sum += i));
+//   if (sum === 10) break;
+// }
+
+// for (i = 1; i < 10; i++) {
+//   console.log(i);
+//   i += i;
+// }
+
+// for (i = 1; i < 10; ) {
+//   console.log(i);
+//   i += i;
+// }
+
+// for (i = 1; i < 10; i++) {
+//   if (i % 3 === 0) {
+//     console.log(i);
+//   } else continue;
+// }
+
+// for (i = 1; i < 10; i++) {
+//   if (i % 3 === 0) continue;
+//   console.log(i);
+// }
+
+// for (i = 1; i < 10; ) {  //Infinite loop
+//   if (i % 3 === 0) continue;
+//   console.log(i);
+//   i++;
+// }
+
+// for (i = 2; i < 10; i++) {
+//   console.log(`${i} lik karra jadvali`);
+//   for (j = 1; j <= 10; j++) {
+//     console.log(`${i} x ${j} = ${i * j}`);
+//   }
+//   console.log(" ");
+// }
+
+//for loopda ichki loop orqali tashqi loopga ham ta'sir o'tkazish uchun
+//label lardan foydalaniladi, Yani:
+
+// outer: for (i = 1; i <= 10; i += 3) {
+//   //bu yerda outer: label hisoblanadi
+//   inner: for (j = 1; j <= 10; j += 3) {
+//     //bu yerda inner: label hisoblanadi. inner ni berish ixtiyoriy
+//     if (i === 4) break;
+//     console.log(j, "j");
+//   }
+//   console.log(i, "(i)");
+// }
+
+//Yuqoridagi misolda ichki loopda agar tashqi loop 4 ga yetganda
+//jarayon to'xtashi kerak, lekin ichki loopda ishlatilgan break
+//jarayonni bir marta to'xtatadi va ichki loop bir marta aylanani o'tkazib
+//yuboradi va jarayon davom etaveradi.
+// agar ichki loopda turib tashqi loopni batamom to'xtatmoqchi bo'lsak
+//quyidagicha kod yoziladi
+
+// outer: for (i = 1; i <= 10; i += 3) {
+//   //bu yerda outer: label hisoblanadi
+//   inner: for (j = 1; j <= 10; j += 3) {
+//     //bu yerda inner: label hisoblanadi. inner ni berish ixtiyoriy
+//     if (i === 4) break outer; //tashqi loopning labeli ko'rsatiladi
+//     console.log(j, "j");
+//   }
+//   console.log(i, "(i)");
+// }
+
+// outer: for (i = 1; i <= 10; i += 3) {
+//   //bu yerda outer: label hisoblanadi
+//   if (i === 4) break;
+//   inner: for (j = 1; j <= 10; j += 3) {
+//     //bu yerda inner: label hisoblanadi. inner ni berish ixtiyoriy
+
+//     console.log(j, "j");
+//   }
+//   console.log(i, "(i)");
+// }
+// let browser = "Safari";
+// switch (browser) {
+//   case "Edge":
+//     console.log("You've got the Edge");
+//     break;
+//   case "Chrome":
+//   case "Firefox":
+//   case "Safari":
+//   case "Opera":
+//     console.log("Ok we support these browser too");
+//     break;
+//   default:
+//     console.log("We hope that this page looks ok!");
+// }
