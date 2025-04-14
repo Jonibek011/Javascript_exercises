770700be69233e34e5085871f3adde6877404d6c
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-14 11:15:09 +0500

    ...

--- While_doWhile_functions/index.js
+++ While_doWhile_functions/index.js
@@ -1,9 +1,113 @@
 //Homework: how many prime numbers in 1 to 100
 // An integer number greater than 1 is called prime if it cannot be devided
 // whithout remainder byanithing exept 1 and itself
-var prime = 0;
-for (i = 1; i <= 100; i++) {
-  if (i % 1 === 0 && i % i === 0) prime += 1;
-}
+// const sum = 50;
 
-console.log(prime);
+// const tubson = (sum) => {
+//   var prime = 0;
+//   for (i = 2; i <= sum; i++) {
+//     let tub = true;
+
+//     for (j = 2; j < i; j++) {
+//       if (i % j == 0) {
+//         tub = false;
+//         break;
+//       }
+//     }
+
+//     if (tub) {
+//       prime += 1;
+//     }
+//   }
+//   console.log(prime);
+// };
+
+// tubson(50);
+
+//==========================================
+// let name = "webbrain";
+// const test = () => {
+//   let name = "Academy"; // varialble shadow
+//   console.log(name);
+// };
+
+// console.log(name);
+// test();
+
+//========= Default parametr =======================
+// Before ESX
+// const telegram = (firstName, lastName) => {
+//   if (lastName) console.log(firstName, lastName);
+//   else console.log(firstName);
+// };
+
+// telegram("webbrain");
+
+// const telegram = (firstName, lastName) => {
+//   lastName ? console.log(firstName, lastName) : console.log(firstName);
+// };
+
+// telegram("webbrain");
+
+// const telegram = (firstName, lastName) => {
+//   console.log(firstName, lastName || "");
+// };
+
+// telegram("webbrain", "academy");
+
+// ==== NOW ====
+// const telegram = (firstName, lastName = "") => {
+//   console.log(firstName, lastName);
+// };
+
+// telegram("webbrain", "academy");
+
+// Question: Rewrite the code changing the for loop to while without alerting its behavior(the output should stay same)
+// for (let i = 0; i < 3; i++) {
+//   console.log(`number ${i}!`);
+// }
+
+//Answer
+// let i = 0;
+// while (i < 3) {
+//   console.log(`number ${i}!`);
+//   i++;
+// }
+
+// do {
+//   console.log(`number ${i}!`);
+//   i++;
+// } while (i < 3);
+
+// Question: The following function returns true if the parametr age is greater than 18.
+//Otherwise it asks for a confirmation and return its result
+// function checkAge(age){
+//   if (age > 18){
+//     return true
+//   }else{
+//     return confirm('Did parents allow you?')
+//   }
+// }
+
+//Rewrite the function using '?' or '||'
+
+// function checkAge(age) {
+//   return age > 18 ? true : "Did parents allow you?";
+// }
+
+// console.log(checkAge(19));
+
+// function checkAge(age) {
+//   return age > 18 || "Did parents allow you?";
+// }
+
+// console.log(checkAge(17));
+
+// return min number
+// function min(a, b) {
+//   // if (a > b) return b;
+//   // else return a;
+
+//   return a > b ? b : a;
+// }
+// console.log(min(2, -2));
