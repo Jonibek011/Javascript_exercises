2f8a0e8d35bd14794a2de1ad7c59a28eb4476f2f
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-15 11:15:24 +0500

    object_basic

--- object_basic/index.js
+++ object_basic/index.js
@@ -97,3 +97,93 @@
 
 // delete account.name;
 // console.log(account.name); // undefined
+
+// ======================= Objectni muzlatish ================================
+//Biz object malumotlarini o'zgarishini oldini olish yoki o'zgartirishni taqiqlab
+//qo'yish uchun uni muzlatib qo'yishimiz mumkin
+// let account = {
+//   name: "Webbrain Academy",
+//   major: "Frontend IT academy",
+//   founded: 2020,
+//   "full name": "Joonibek Orifjonov",
+//   0: 156,
+// };
+
+// // Object.freeze(account); // bunda objectga ma'lumot qo'shib, o'zgartirib yoki o'chirib bo'lmaydi
+// // bu muzlatish gaqat shu koddan keyin yozilgan o'zgartirishlarga amal qiladi
+// // bu koddan yuqorida objectni hohlagancha o'zgartirish mumkin
+
+// Object.seal(account); // bu freeze dan farqli ravishda malumot qo'shib yoki o'chirib bo'lmaydi ammo obj dagi mavjud malumotni o'zgartirish mumkin
+
+// //object malumotlari muzlatilgan yoki seal qilinganligini tekshirish mumkin
+// console.log(Object.isFrozen(account));
+
+// console.log(Object.isSealed(account));
+
+// ============= OBJECTDAN NUSXA OLISH =================================
+
+// objectdan nusxa olishda yuqorida ko'rilganidek obj3 = obj1 qilinsa obj3 hamda obj1
+//bitta manzilga bog'lanib qoladi hamda bittasining qiymati o'zgarsa albatta ikkinchisining
+//ham qiymati o'zgaradi shunday xolat bo'lmasligi uchun quyidagicha deep copy qilinadi
+
+// const obj1 = {name:1, surename:2}
+
+// const obj2 = structuredClone(obj1)
+
+//structuredClone ishlatilganda obj1 obj2 ga full copy boladi. birining qiymati o'zgarganda ikkinchisiniki o'zgarmaydi
+//structuredClone node ning 17 yoki undan yuqori versiyalarida ishlaydi
+
+// ==================================================================================
+//object valuesini tashqarida elon qilingan o'zgaruvchiga almashtirish mumkin
+//Masalan:
+
+// let name = "Webbrain Academy";
+// let major = "Frontend";
+
+// const obj = {
+//   name: name,
+//   major: major,
+// };
+
+// //object qoidalariga ko'ra agar value hamda key ning qiymati bir xil bo'lsa ularni qisqartirib faqat bittasini qoldirish mumkin
+// //Masalan:
+
+// const obj1 = {
+//   name,
+//   major,
+// };
+
+// ======================================================================================
+//objectning ichida qaysidir key bor yoki yoo'qligini aniqlash mumkin
+
+// const obj = {
+//   name: "Webbrain",
+//   major: "Frontend",
+// };
+
+// console.log("name" in obj); // agar obj ichida name nomli key bo'lsa true qaytaradi
+
+// ===============================================================================
+//Object key va valuelarini for loop orqali olish
+
+// const object = {
+//   name: "Jonibek",
+//   surename: "Orifjonov",
+//   age: 29,
+//   major: "Frontend",
+//   profession: "React",
+// };
+
+// for (let i in object) { // bu usulda objectdagi keylar olinadi
+//   console.log(i);
+// }
+
+// for (let i in object) {
+//   console.log(object[i]);  // bu objectning valuelarini olishda ishlatiladi
+// }
+
+// const values = Object.values(object); // object valuelarini array sifatida qaytaradi
+
+// for (let i = 0; i < values.length; i++) {
+//   console.log(values[i]);
+// }
