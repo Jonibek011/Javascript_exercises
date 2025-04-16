62fc6db57a70ad7dcfcea9f1ac735e9dc38b9c33
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-16 17:40:28 +0500

    numbers

--- numbers/index.js
+++ numbers/index.js
@@ -1,1 +1,8 @@
 //Numbers
+
+// katta miqdordagi numberlarni o'qishga osonroq bo'lishi uchun underscore dan foydalaniladi
+console.log(100_000_000); // =100000000
+// 0 lar sonini qisqartirish uchun birinchi raqamdan keyin e yozib keyin 0 lar soni yoziladi
+console.log(1e6); // 1 000 000
+console.log(1e6 + 1); // 1 000 001
+console.log(1e-6); // 0.000 001 
