f3cf40d12381a3939c703cdec0dd25c6b53d028e
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-17 14:09:49 +0500

    Binary and getResult

---
+++ number_exercises/index.js
@@ -0,0 +1,25 @@
+// Berilgan raqamni bynary ga o'tkazib unda nechta 0 va nechta 1 borligini qaytaruvchi funksiya yozing
+
+// function countBinary(num) {
+//   let bnum = num.toString(2);
+
+//   let all1 = 0;
+//   let all0 = 0;
+
+//   for (let i of bnum) {
+//     if (i == 1) {
+//       all1++;
+//     } else all0++;
+//   }
+//   console.log(`1 lar soni ${all1} ta, 0 lar soni ${all0}`);
+// }
+
+// countBinary(20);
+
+// Berilgan arraydagi raqamlarni eng katta va eng kichigini ayirmasini qaytaruvchi funksiya yozing
+
+// const arr = [4, 9, 15, 18, 42];
+
+// const getResult = (arr) => Math.max(...arr) - Math.min(...arr);
+
+// console.log(getResult(arr));
