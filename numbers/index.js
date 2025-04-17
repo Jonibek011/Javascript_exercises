663c865dfc00b1e2c54333f3643c257acd138b0e
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-17 09:31:42 +0500

    toFixed()

--- numbers/index.js
+++ numbers/index.js
@@ -1,8 +1,19 @@
 //Numbers
 
 // katta miqdordagi numberlarni o'qishga osonroq bo'lishi uchun underscore dan foydalaniladi
-console.log(100_000_000); // =100000000
-// 0 lar sonini qisqartirish uchun birinchi raqamdan keyin e yozib keyin 0 lar soni yoziladi
-console.log(1e6); // 1 000 000
-console.log(1e6 + 1); // 1 000 001
-console.log(1e-6); // 0.000 001 
+// console.log(100_000_000); // =100000000
+// // 0 lar sonini qisqartirish uchun birinchi raqamdan keyin e yozib keyin 0 lar soni yoziladi
+// console.log(1e6); // 1 000 000
+// console.log(1e6 + 1); // 1 000 001
+// console.log(1e-6); // 0.000 001
+
+// toFixed(parametr) - bu metod float sonlarni nuqtadan keyin nechta raqamini qaytarishini belgilaydi.
+//parametr ga berilgan raqamga teng ravishda nuqtadan keyingi raqamlarni qaytaradi
+
+// let num = 0.1234;
+// console.log(num.toFixed(2)); // 0.12
+
+//Kamchiliklari
+//1) qaytargan qiymatini yaxlitlab qaytaradi
+//2) strining qiymat qaytaradi
+// 3) string numberlar ustida ishlay olmaydi
