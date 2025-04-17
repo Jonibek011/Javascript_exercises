c19cf05f79683d56682c3098b5f1c674796ddd33
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-17 12:52:35 +0500

    Math

--- numbers/index.js
+++ numbers/index.js
@@ -86,9 +86,9 @@
 // toPrecision() - bu metod parametriga nechi sonini olsa berilgan raqamning shuncha raqamini qaytaradi
 // agar berilgan raqam 0 dan kichik bolsa 0. dan keyin birinchi 0 bo'lmagan raqamdan boshlab qayradi
 
-let num = 12345;
-let num1 = 0.12345;
-let num2 = 0.00012345;
+// let num = 12345;
+// let num1 = 0.12345;
+// let num2 = 0.00012345;
 
 // console.log(num.toPrecision(3)); // 1.23e+4
 // console.log(num1.toPrecision(3)); // 0.123
@@ -99,3 +99,63 @@ let num2 = 0.00012345;
 // Kamchiligi
 //1) string qiymat qaytaradi
 //2) intiger sonlarda scientific numberlar qaytaradi va raqamni floatga aylantiradi
+
+// ===========================================================================
+
+// Math - bu javascript build in kutubxona ya'ni javascriptning o'zida joylashgan kutubxona
+// uning birqancha metodlari mavjud
+
+// console.log(Math.PI); // PI ning to'liq qiymatini qaytaradi
+
+//Math.abs() - berilgan sonning musbat qismini qaytarib beradi yoki musbatga o'girib beradi
+
+// let num = -123;
+// console.log(Math.abs(num)); //123
+
+// Math.round() berilgan float raqamni yaxlitlab integerga aylantirib qaytaradi, yaxlitlash nuqtadan keyingi sonning qiymatiga bog'liq
+
+// let num = 3.4;
+// let num1 = 3.5;
+
+// console.log(Math.round(num)); // 3
+// console.log(Math.round(num1)); //4
+
+//Math.floor() = berilgan float raqamni pastga qarab yaxlitlaydi
+
+// console.log(Math.floor(3.8)) // 3
+// console.log(Math.floor(-3.8)) // -4
+
+//Math.ceil() = berilgan float raqamni o'zak qismidan yuqori raqamga yaxlitlaydi
+// console.log(Math.ceil(3.01)); //4
+
+//Math.trunc() = floor ga o'xshash lekin bu metod berilgan float raqamni float qismini olib tashlab faqat o'zak qismini qaytaradi
+// console.log(Math.trunc(3.8)) // 3
+// console.log(Math.trunc(-3.8)) // -3
+
+//Math.random() = 0 dan 1 gacha bo'lgan ixtiyoriy raqamlar ketma ketligini qaytaradi
+
+// console.log(Math.random()); // ixtiyoriy raqam
+// randomdan unumli foydalanish uchun matematik amallardan foydalanish kerak
+
+// console.log(Math.trunc(Math.random() * 10));
+//Math.trunc ishlatilganda 10 raqami hech qachon chiqmaydi agar 10 raqami ham chiqishini istasak ceildan foydalanishimiz mumkin
+// console.log(Math.ceil(Math.random() * 10));
+
+//Math.min() = parametrga berilgan raqamlarning ichidan eng kichigini qaytaradi
+
+// console.log(Math.min(3, 5, 32, 10, 21));
+
+// array yoki tyuple listdagi raqamlarni ichidan eng kichigini aniqlay olmaydi faqat parametriga berilgan raqamni ichidan aniqlaydi
+// let num = (3, 5, 32, 10, 21); // x
+
+//Math.max() = parametriga berilgan raqamlar ichidan eng kattasini qaytaradi
+
+//Math.pow(par1, par2) = birinchi parametrdagi raqamni ikkinichi parametrdagi daraga oshiradi
+
+// console.log(2, 3); //8
+
+// Math.sqrt() berilgan sonning kvadrat ildizini qaytaradi
+// Math.cbrt() berilgan sonning kub ildizini qaytaradi
+
+// console.log(Math.sqrt(9)); //3
+// console.log(Math.cbrt(27)); //3
