ab80856ad5291e8080e33f39fa627c3d96f41cae
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-17 17:46:29 +0500

    reverse number

--- number_exercises/index.js
+++ number_exercises/index.js
@@ -36,3 +36,62 @@
 // };
 
 // randomNumber(30, 50);
+
+// Berilgan sonning faqat butun qismini oladigan funksiya yozing
+
+// function floorNumber(num) {
+//   return Math.floor(num);
+// }
+
+// floorNumber(4.8);
+
+// Berilgan sonni 2 xonali kasr ko'rinishida qirqib chiqaradigan funksiya yozing.
+
+// function FixedToDigits(num) {
+//   return num.toFixed(2);
+// }
+
+// console.log(FixedToDigits(7));
+
+// Sonni 3 xonali aniqlikda chiqaradigan funksiya yozing.
+
+// const precisionNumber = (num) => num.toPrecision(3);
+// console.log(precisionNumber(1.2345));
+
+// Berilgan butun sonni har bir raqamiga ajratib, ularning yig'indisini topuvchi funksiya yoz.
+// const sumNumber = (num) => {
+//   let strNum = String(num);
+//   let sum = 0;
+//   for (let i of strNum) {
+//     sum += +i;
+//   }
+//   return sum;
+// };
+
+// console.log(sumNumber(1234));
+
+// Berilgan ikkita sonni oladigan va ikkalasining o'rtacha qiymatini qaytaradigan funksiya yoz.
+
+// const average = (a, b) => (a + b) / 2;
+
+//Berilgan sonni tekshirib, u musbat, manfiy yoki nolga tengligini aniqlab beradigan funksiya yoz.
+
+// const checkNumber = (num) => {
+//   let message = "";
+//   if (num === 0) {
+//     message = "0 ga teng";
+//   } else if (num > 0) {
+//     message = "Musbat";
+//   } else message = "Manfiy";
+
+//   return message;
+// };
+
+// console.log(checkNumber(-3));
+
+const reverseNumber = (num) => {
+  let strNum = String(num);
+  return Number(strNum.split("").reverse().join(""));
+};
+
+reverseNumber(12345);
