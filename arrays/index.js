ab7e2904bda848097e64f6d6367e9d8bf41b9faa
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-24 15:45:10 +0500

    array.length

--- arrays/index.js
+++ arrays/index.js
@@ -67,8 +67,12 @@
 
 // ====================================================================
 //Arrayni loop qilish
-let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 
-for (let i of arr) {
-  console.log(i);
-}
+// for (let i of arr) {
+//   console.log(i);
+// }
+
+//============== Array metodlari =========================================
+
+//.length - arrayni uzunligini qaytaradi
