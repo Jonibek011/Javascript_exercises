fff51462acf395373031355dab0266dc55bac192
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-27 21:10:36 +0500

    shallow copy

--- object_advanced/index.js
+++ object_advanced/index.js
@@ -63,10 +63,29 @@
 //Object.assign() = bitta yoki bir nechta objectni bitta yangi objectga nusxalash yoki birlashtirish uchun ishlatiladi
 
 //Object.assign(target, ...sources)
+//agar target objectga sources objectlardan bir xil nomdagi property kelsa oxirgisi ustunlik qiladi
+// Object.assign() shallow copy qiladi(yani faqat birinchi qatlamni nusxalaydi)
 
-const user = { name: "Ali" };
-const info = { age: 25 };
-const address = { city: "Tashkent" };
+// const user = { name: "Ali" };
+// const info = { age: 25 };
+// const address = { city: "Tashkent" };
 
-const newUser = Object.assign({}, user, info, address);
-console.log(newUser);
+// const newUser = Object.assign({}, user, info, address);
+// console.log(newUser);
+
+// let obj = { name: "Ali", age: 3, address: { city: "Fergana" } };
+
+// let newObj = Object.assign({}, obj);
+// newObj.address.city = "Samarqand";
+// console.log(newObj); //{ name: 'Ali', age: 3, address: { city: 'Samarqand' } }
+// console.log(obj); //{ name: 'Ali', age: 3, address: { city: 'Samarqand' } }
+
+//==============================================================
+//slice(), spread operator, Array.from() hamda Object.assign() lar objectni shallow copy qiladi
+// yani agar nusxa olinayotgan object ichida nested objectlar bo'lsa nested objectlarning referensini
+//yani xotirada joylashgan manzilini nusxalaydi, natijada nested bo'lmagan key value larning qiymatlari yangi
+//objectga butunlay ko'chadi yangi objectdagi ushbu qiymatlar o'zgartirilsa eski object o'zgarmaydi,
+// nested objectlarning manzili ko'chib o'tganligi uchun yangi object chaqirilganda ko'chib o'tgan reference
+// eski objectning nested objecti joylashgan reference ga murojaat qiladi va qiymat qaytaradi
+// oqibatda yangi xosil qilingan objectdagi nested object o'zgartirilsa shu referenceda joylashgan
+//eski objectdagi nested object ham o'zgaradi yani eski object ham o'zgaradi
