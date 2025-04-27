57c88efb8112d574681d80d07f3a12ddbe4a7cf5
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-04-27 18:10:39 +0500

    .set()

--- Map/index.js
+++ Map/index.js
@@ -7,10 +7,35 @@
 
 //mapga boshlang'ich qiymat ham berilishi mumkin, lekin kalit va qiymatlar array ichida array shaklida beriladi
 
-const newMap = new Map([
-  ["name", "Ali"],
-  ["age", 3],
-  ["isStudend", false],
-]);
+// const newMap = new Map([
+//   ["name", "Ali"],
+//   ["age", 3],
+//   ["isStudend", false],
+// ]);
 
-console.log(newMap);
+// console.log(newMap); //Map(3) { 'name' => 'Ali', 'age' => 3, 'isStudend' => false }
+
+//===========================================================================
+
+// set() - mapga malumot qo'shish uchun ishlatiladi
+
+const newMap = new Map();
+newMap.set("meva", "olma");
+newMap.set("animal", "horse");
+newMap.set("animal", "horse"); // Map ham Set singari bir hil qiymatlarni faqat bir marta saqlaydi, takrorlanuvchi qiymatlarni saqlamaydi
+
+console.log(newMap); // Map(2) { 'meva' => 'olma', 'animal' => 'horse' }
+
+newMap.set("animal", "dog");
+console.log(newMap); //Map(2) { 'meva' => 'olma', 'animal' => 'dog' }
+// demak Map bir hil qiymatlarni faqat bir marta saqlaydi
+//undan tashqari bir xil key ga egan bo'lgan ikki qiymat berilsa ularning faqat oxirgi berilganini saqlaydi
+// keylar unique bo'lishi kerak
+
+newMap.set("hayvon", "horse");
+console.log(newMap); //Map(3) { 'meva' => 'olma', 'animal' => 'dog', 'hayvon' => 'horse' }
+// ko'rinib turganidek Mapda vaqat keylar unique bo'lishi kerak, qiymatlar takrorlanishi mumkin
+
+const obj = { key: "salom", key: 3 };
+console.log(obj); //{ key: 3 }
+//malumot uchun objectlarda ham key unique bo'lishi kerak ekan va bir keyning oxirgisini qabul qilar ekan
