5ae26ae5f6a0acadf4f1e6c898db3173dcca429d
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-05-06 17:00:29 +0500

    class dan meros olish

--- class_basic/index.js
+++ class_basic/index.js
@@ -23,3 +23,36 @@ const user = new Person("Jonibek", 28);
 
 //B) Metodlar
 //--class ichida yozilgan funksiyalar avtomatik prototypega qo'shiladi
+
+//========================================================
+//Class lar orqali meros olish
+//Asosiy tushuncha: extends va super
+// extends - bir class boshqa class dan meros olishini bildiradi
+// super - parent classga murojaat qilish uchun ishlatiladi
+
+class Animal {
+  // ota class
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log(`${this.name} makes a noise`);
+  }
+}
+
+class Dog extends Animal {
+  //bola class
+  speak() {
+    console.log(`${this.name} barks`);
+  }
+
+  dringk() {
+    console.log(`${this.name} drinks milk`);
+  }
+}
+
+const rex = new Dog("Rex");
+
+rex.speak(); // Rex barks
+rex.dringk(); // Rex drinks milk
