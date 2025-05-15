d0068f4df9d3533a0afeecf27fa39ca830976efe
Jonibek Orifjonov  Web Developer <147488141+Jonibek011@users.noreply.github.com>
2025-05-15 18:48:14 +0500

    generator functions

--- generator_functions/index.js
+++ generator_functions/index.js
@@ -4,20 +4,20 @@
 //generatorlar esa pauzali ishlaydi: ular yield operatori yordamida bajarilishini to'xtatadi va keyinchalik davom ettiradi
 
 //Sintaksis
-function* generatorFunction() {
-  yield 1;
-  yield 2;
-  yield 3;
-}
+// function* generatorFunction() {
+//   yield 1;
+//   yield 2;
+//   yield 3;
+// }
 
 //etibor bering: function * deb yoziladi
 //generator funksiyani chaqirish iterator obyektni qaytaradi
 
-const gen = generatorFunction();
-console.log(gen.next());
-console.log(gen.next());
-console.log(gen.next());
-console.log(gen.next());
+// const gen = generatorFunction();
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
+// console.log(gen.next());
 
 //yani funksiya yield operatori bilan 3 qismga bo'lib yozilsa
 // funksiya uch marta chaqirilsa har bir yieldga yozilgan qism obyect sifatida qaytadi
@@ -25,19 +25,32 @@ console.log(gen.next());
 
 //yield nima?
 //yield -bu generator funksiyasi ichida ishlatiladigan maxsus operator bo'lib, u qiymatni tashqariga uzatadi va bajarilishni to'xtatadi
-function* fruitGenerator() {
-  yield "Apple";
-  yield "Banana";
-  yield "Cherry";
-}
+// function* fruitGenerator() {
+//   yield "Apple";
+//   yield "Banana";
+//   yield "Cherry";
+// }
 
-const fruits = fruitGenerator();
-console.log(fruits.next()); //{ value: 'Apple', done: false }
-console.log(fruits.next().value); //Apple
-console.log(fruits.next().value); // Banana
+// const fruits = fruitGenerator();
+// console.log(fruits.next()); //{ value: 'Apple', done: false }
+// console.log(fruits.next().value); //Apple
+// console.log(fruits.next().value); // Banana
 
 //Done nima?
 //har bir nex() chaqirig'i quyidagi objectni qaytaradi:
 // {value:..., done: true/false}
 // value - yield chaqirilgan qiymat
 // done - generator tugaganmi yoki yo'qmi (true bolsa -generator oxiriga yetgan)
+
+//Generatorlarni for....of bilan ishlatish
+function* countToThree() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+
+for (count of countToThree()) {
+  console.log(count);
+}
+
+//demak generator funksiyalarni for of bilan loop qilganimizda uni next() metodini chaqirishimiz shart emas
